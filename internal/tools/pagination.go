a5254224b19a77e365ef8f45cb6426286fb9f5a1
pieceowater <pieceowater@gmail.com>
2024-09-29 18:30:21 +0500

    ToPaginated fix

--- internal/tools/pagination.go
+++ internal/tools/pagination.go
@@ -12,19 +12,12 @@ type PaginatedEntity[T any] struct {
 }
 
 // ToPaginated converts raw data (a slice of entities and count) to a PaginatedEntity.
-// The first element of data is expected to be the list of entities and the second element is the total count.
-func ToPaginated[T any](data []any) PaginatedEntity[T] {
-	// Convert the raw data[0] to a slice of T
-	items := make([]T, len(data[0].([]any)))
-	for i, v := range data[0].([]any) {
-		items[i] = v.(T) // Type assertion
-	}
-
+func ToPaginated[T any](items []T, count int) PaginatedEntity[T] {
 	// Return a PaginatedEntity with the rows and entity count
 	return PaginatedEntity[T]{
 		Rows: items,
 		Info: EntityInfo{
-			Count: data[1].(int), // Assuming the second element is the total count
+			Count: count,
 		},
 	}
 }
