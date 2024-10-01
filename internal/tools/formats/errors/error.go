8a37257372ab0dc8eda749e3fa3911b35ad50ece
pieceowater <pieceowater@gmail.com>
2024-10-01 15:47:42 +0500

    ServiceError

--- internal/tools/formats/errors/error.go
+++ internal/tools/formats/errors/error.go
@@ -1,8 +1,13 @@
 package errors
 
+import (
+	"net/http"
+)
+
 // ServiceError represents a custom error used throughout the application.
 type ServiceError struct {
-	Message string // The error message
+	StatusCode int    `json:"statusCode"`
+	Message    string `json:"message"`
 }
 
 // Error returns the error message, making ServiceError comply with the error interface.
@@ -10,17 +15,22 @@ func (e *ServiceError) Error() string {
 	return e.Message
 }
 
-// GetError returns a map representing the error name and message.
-func (e *ServiceError) GetError() map[string]string {
-	return map[string]string{
-		"name":    "ServiceError",
-		"message": e.Message,
+// GetError returns a map representing the error status code and message.
+func (e *ServiceError) GetError() map[string]interface{} {
+	return map[string]any{
+		"message":    e.Message,
+		"statusCode": e.StatusCode,
 	}
 }
 
-// NewServiceError creates a new ServiceError instance with the given message.
-func NewServiceError(message string) *ServiceError {
+// NewServiceError creates a new ServiceError instance with the given message and status code.
+func NewServiceError(message string, statusCode ...int) *ServiceError {
+	code := http.StatusInternalServerError
+	if len(statusCode) > 0 {
+		code = statusCode[0]
+	}
 	return &ServiceError{
-		Message: message,
+		Message:    message,
+		StatusCode: code,
 	}
 }
